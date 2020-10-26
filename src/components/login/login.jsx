70438848ae37ad181749e9ba18e158c9a4c5e6c0
feel2174 <feel2174@gmail.com>
2020-10-26 19:59:14 +0900

    project set up & add firebase

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
 import styles from './login.module.css'
 
-const Login = (props) => {
-    const loginGoogle = () => {
-        console.log('google');
-    }
-    const loginGithub =() => {
-        console.log('github');
-    }
+
+const Login = () => {
     return (
         <div>
         <h1>Login</h1>
-        <button className={styles.button} onClick={loginGoogle}>Google</button>
-        <button className={styles.button} onClick={loginGithub}>Github</button>
+        <button className={styles.button} >Google</button>
+        <button className={styles.button} >Github</button>
         </div>
     )
 }
