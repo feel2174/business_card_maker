0100ae130c1d1726907613f29329cbbfb9b880c5
feel2174 <feel2174@gmail.com>
2020-11-02 16:09:56 +0900

    Updates

---
+++ precache-manifest.5bc967d56075318fe706927d322ab2fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06d2ba6d44064a79a58cd36739d072b1",
+    "url": "/business_card_maker/index.html"
+  },
+  {
+    "revision": "e67a9d16a42302edc2db",
+    "url": "/business_card_maker/static/css/main.d79baa88.chunk.css"
+  },
+  {
+    "revision": "86e4dee9468f62fb2b0a",
+    "url": "/business_card_maker/static/js/2.be2235fd.chunk.js"
+  },
+  {
+    "revision": "7d2282234cd0247bd4712fb2834fc8d9",
+    "url": "/business_card_maker/static/js/2.be2235fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e67a9d16a42302edc2db",
+    "url": "/business_card_maker/static/js/main.f9e9add3.chunk.js"
+  },
+  {
+    "revision": "84e16d80aeae1bb1632f",
+    "url": "/business_card_maker/static/js/runtime-main.738df246.js"
+  }
+]);
