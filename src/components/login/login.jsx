1cbd3441a36798732837686d2ff8c34875fff21c
Young Joo Kim <37773812+feel2174@users.noreply.github.com>
2020-11-03 14:18:36 +0900

    Integration (#2)
    * project set up & add firebase
    
    * add login component
    
    * added main screen
    
    * Add editor and preview layout
    
    * add card and edit form component
    
    * add loading spinner for file upload
    
    * warning & firebase clean up
    
    * fix typo & optimization
    
    * deleted firebase api in index.html

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -14,7 +14,9 @@ const Login = ({ authService }) => {
   }
   
   const onLogin = (event) => {
-    authService.login(event.currentTarget.textContent)
+
+    authService //
+    .login(event.currentTarget.textContent)
     .then(data => goToMain(data.user.uid));
   };
 
